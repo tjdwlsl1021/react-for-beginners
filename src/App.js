19670b4f2ab65ee5c4fe244c6621ddc3fd27fe26
seongjin.hwang <tjdwlsl1021@gmail.com>
2022-02-17 13:33:41 +0900

    #6.2 Deps

--- src/App.js
+++ src/App.js
@@ -3,13 +3,34 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword]= useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the time");
-  useEffect(() => {
-    console.log("CALL the API....")
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => { // 한번만 실행
+    console.log("I run only once.")
   }, []);
+
+  useEffect(() => { // keyword변경시 실행
+    if(keyword !== "" && keyword.length > 5) {
+      console.log("SEARCH FOR", keyword)
+    }
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.")
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.")
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when keyword & counter changes.")
+  }, [keyword, counter]);
+
   return (
     <div>
+      <input 
+      value={keyword}
+      onChange={onChange} type="text" placeholder="Search here..."></input>
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
