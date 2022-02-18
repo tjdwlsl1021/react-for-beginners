0aa6a8e19b8f4673b9e0f05a59f65a580464f0d9
seongjin.hwang <tjdwlsl1021@gmail.com>
2022-02-18 16:51:42 +0900

    #7.3 Movie App part Two

--- src/App.js
+++ src/App.js
@@ -3,27 +3,52 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([])
+  const [movies, setMovies] = useState([]);
+
+  // 방법1
+  // useEffect(() => {
+  //   fetch(
+  //     `https://yts.mx/api/v2/list_movies.json?minimun_rating=8.5&sort_by=year`
+  //   )
+  //     .then((response) => response.json())
+  //     .then((json) => {
+  //       setMovies(json.data.movies);
+  //       setLoading(false);
+  //     });
+  // }, [])
+  // console.log(movies);
+
+  // 방법2
+  const getMovies = async () => {
+    const json = await (await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimun_rating=8.5&sort_by=year`
+    )).json();
+    // const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
+  console.log(movies)
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? <strong>Loading...</strong> : (
-        <select>
-          {coins.map((coin) => (
-            <option>
-              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
-            </option>
+      {loading ? (<h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map(movie => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map(g => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
           ))}
-        </select>)}
-
+        </div>)}
     </div>
   );
 }
