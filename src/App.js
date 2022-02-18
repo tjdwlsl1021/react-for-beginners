20adc15a019246101a8c30b4b79e18d834adc046
seongjin.hwang <tjdwlsl1021@gmail.com>
2022-02-18 09:18:07 +0900

    #6.4 Cleanup

--- src/App.js
+++ src/App.js
@@ -1,40 +1,47 @@
 
 import { useState, useEffect } from "react";
 
+// function Hello() {
+//   useEffect(() => {
+//     console.log('created');
+//     return () => console.log('destroyed');
+//   }, []) // [] <- 없으면 한번만 실행
+//   return <h1>Hello</h1>;
+// }
+
+
+function Hello() {
+  // function byFn() {
+  //   console.log('bye');
+  // }
+  // function hiFn() {
+  //   console.log('created');
+  //   return byFn; // destroy될 때 retrun호출
+  // }
+  // useEffect(hiFn, []) // [] <- 없으면 한번만 실행
+
+  useEffect(() => { // 사용방법1
+    console.log("hi");
+    return () => console.log("bye");
+  }, [])
+
+  // useEffect(function() { // 사용방법2
+  //   console.log("hi");
+  //   return function() {
+  //     console.log("bye");
+  //   }
+  // }, [])
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword]= useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => { // 한번만 실행
-    console.log("I run only once.")
-  }, []);
-
-  useEffect(() => { // keyword변경시 실행
-    if(keyword !== "" && keyword.length > 5) {
-      console.log("SEARCH FOR", keyword)
-    }
-  }, [keyword]);
-
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.")
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes.")
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when keyword & counter changes.")
-  }, [keyword, counter]);
-
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input 
-      value={keyword}
-      onChange={onChange} type="text" placeholder="Search here..."></input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
-
 export default App;
