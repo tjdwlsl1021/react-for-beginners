d1447a106182fbbc63cee596f47a5c40ad2a0826
seongjin.hwang <tjdwlsl1021@gmail.com>
2022-02-18 09:48:35 +0900

    #7.1 To Do List part One

--- src/App.js
+++ src/App.js
@@ -1,46 +1,31 @@
 
 import { useState, useEffect } from "react";
 
-// function Hello() {
-//   useEffect(() => {
-//     console.log('created');
-//     return () => console.log('destroyed');
-//   }, []) // [] <- 없으면 한번만 실행
-//   return <h1>Hello</h1>;
-// }
-
-
-function Hello() {
-  // function byFn() {
-  //   console.log('bye');
-  // }
-  // function hiFn() {
-  //   console.log('created');
-  //   return byFn; // destroy될 때 retrun호출
-  // }
-  // useEffect(hiFn, []) // [] <- 없으면 한번만 실행
-
-  useEffect(() => { // 사용방법1
-    console.log("hi");
-    return () => console.log("bye");
-  }, [])
-
-  // useEffect(function() { // 사용방법2
-  //   console.log("hi");
-  //   return function() {
-  //     console.log("bye");
-  //   }
-  // }, [])
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setTodo] = useState("");
+  const [toDos, setTodos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    // console.log(toDo);
+    if (toDo === "") {
+      return;
+    }
+    setTodos((currentArray) => [toDo, ...currentArray]);
+    setTodo("");
+  }
+  console.log(toDos);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos {toDos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..." />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
