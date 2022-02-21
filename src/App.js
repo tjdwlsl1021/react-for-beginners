3182ff59e4d7a2a078351b2d9287ad17c4f060e7
seongjin.hwang <tjdwlsl1021@gmail.com>
2022-02-21 13:31:47 +0900

    #7.4 Movie App part Two

--- src/App.js
+++ src/App.js
@@ -1,55 +1,8 @@
 
 import { useState, useEffect } from "react";
+import Movie from "./components/Movie";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-  // 방법1
-  // useEffect(() => {
-  //   fetch(
-  //     `https://yts.mx/api/v2/list_movies.json?minimun_rating=8.5&sort_by=year`
-  //   )
-  //     .then((response) => response.json())
-  //     .then((json) => {
-  //       setMovies(json.data.movies);
-  //       setLoading(false);
-  //     });
-  // }, [])
-  // console.log(movies);
-
-  // 방법2
-  const getMovies = async () => {
-    const json = await (await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimun_rating=8.5&sort_by=year`
-    )).json();
-    // const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  console.log(movies)
-  return (
-    <div>
-      {loading ? (<h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map(movie => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map(g => (
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>)}
-    </div>
-  );
+  return null;
 }
 export default App;
